ac8a92f15f2647a6b7bf7f7072551f1d2d8dfcc7
lmorinn <trin123123m@gmail.com>
2024-06-25 17:21:38 +0900

    fix structure

---
+++ hello/AB.hpp
@@ -0,0 +1,5 @@
+#pragma once
+
+long long AplusB(long long a, long long b) {
+  return a + b;
+}
--- test/verify/AB.test.cpp
+++
@@ -1,9 +0,0 @@
-#define PROBLEM "https://judge.yosupo.jp/problem/aplusb"
-#include <iostream>
-using namespace std;
-
-int main() {
-  long long a, b;
-  cin >> a >> b;
-  cout << a + b << endl;
-}
